a0451f02ad3f2d0b8a7a96a7e45284a95b8a0e7a
sbouaouid <salmabouaouid57@gmail.com>
2021-06-08 11:04:54 +0100

    Models and migrations

--- models/user.js
+++ models/user.js
@@ -4,25 +4,23 @@ const {
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   class User extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
+    static associate(models){
+      User.hasMany(models.Article)
     }
   };
+
   User.init({
     username: {
       type: DataTypes.STRING,
       allowNull: false
     },
+
     email: {
       type: DataTypes.STRING,
       unique: true,
       allowNull: false
       },
+      
     password: DataTypes.STRING,
     role: DataTypes.STRING
   }, {
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,9 @@
-var express = require('express');
-var router = express.Router();
+const router = require('express').Router();
+const usersRepo = require('../repositories/users')
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', async function(req, res, next) {
+  res.send(await usersRepo.getAllUsers())
 });
 
 module.exports = router;
